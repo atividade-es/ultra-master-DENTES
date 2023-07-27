d4dd595639877ca21e69d9967b41db07841e5b15
Lucas Branco Alves de Melo <lucasbranco027@protonmail.com>
2023-07-27 17:00:48 -0300

    Render host permitted

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
 
+# Enable render host
+Rails.application.config.hosts << "https://ultra-master-dentes-web.onrender.com/"
+
 # Initialize the Rails application.
 Rails.application.initialize!
