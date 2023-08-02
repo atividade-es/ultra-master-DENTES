eee1fe6a4f1c47965b3637cf35bd01bacf1aed6e
Nidhoggui <guilhermemelquiades42@gmail.com>
2023-08-01 22:50:54 -0300

    dentist CRUD added.

---
+++ app/controllers/dentists_controller.rb
@@ -0,0 +1,49 @@
+class DentistsController < ApplicationController
+  before_action :set_dentist, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @dentists = Dentist.all
+  end
+
+  def show
+  end
+
+  def new
+    @dentist = Dentist.new
+  end
+
+  def create
+    @dentist = Dentist.new(dentist_params)
+    if @dentist.save
+      redirect_to @dentist, notice: 'Dentist was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @dentist.update(dentist_params)
+      redirect_to @dentist, notice: 'Dentist was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dentist.destroy
+    redirect_to dentists_url, notice: 'Dentist was successfully destroyed.'
+  end
+
+  private
+
+  def set_dentist
+    @dentist = Dentist.find(params[:id])
+  end
+
+  def dentist_params
+    params.require(:dentist).permit(:nome, :especializacao, :disponivel_segunda, :disponivel_terca, :disponivel_quarta, :disponivel_quinta, :disponivel_sexta, :disponivel_sabado, :disponivel_domingo)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :patients
+  resources :dentists
   root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
