cad1142af2b418287b1e720a750d857581de62e4
Izabel Nascimento <izabelyale@hotmail.com>
2023-08-04 00:15:13 -0300

    fix add dentist test

--- db/schema.rb
+++ db/schema.rb
@@ -21,19 +21,61 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_03_031037) do
     t.index ["dentista_id"], name: "index_atendimentos_on_dentista_id"
     t.index ["paciente_id"], name: "index_atendimentos_on_paciente_id"
   end
-  
+
+  create_table "consulta", force: :cascade do |t|
+    t.string "data"
+    t.string "hora"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "paciente_id", null: false
+    t.integer "medico_id", null: false
+    t.index ["medico_id"], name: "index_consulta_on_medico_id"
+    t.index ["paciente_id"], name: "index_consulta_on_paciente_id"
+  end
+
   create_table "dentists", force: :cascade do |t|
     t.string "nome"
     t.string "especializacao"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "disponivel_segunda", default: 0
+    t.integer "disponivel_segunda", default: 1
     t.integer "disponivel_terca", default: 0
     t.integer "disponivel_quarta", default: 0
     t.integer "disponivel_quinta", default: 0
     t.integer "disponivel_sexta", default: 0
     t.integer "disponivel_sabado", default: 0
     t.integer "disponivel_domingo", default: 0
+  end
+
+  create_table "enderecos", force: :cascade do |t|
+    t.integer "cep"
+    t.string "cidade"
+    t.string "logradouro"
+    t.string "complemento"
+    t.string "bairro"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "paciente_id", null: false
+    t.index ["paciente_id"], name: "index_enderecos_on_paciente_id"
+  end
+
+  create_table "medicos", force: :cascade do |t|
+    t.string "nome_completo"
+    t.integer "cpf"
+    t.string "email"
+    t.string "especialidade"
+    t.integer "crm"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pacientes", force: :cascade do |t|
+    t.string "nome_completo"
+    t.string "data_nascimento"
+    t.integer "cpf"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "patients", force: :cascade do |t|
@@ -46,4 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_03_031037) do
 
   add_foreign_key "atendimentos", "dentista", column: "dentista_id"
   add_foreign_key "atendimentos", "pacientes"
+  add_foreign_key "consulta", "medicos"
+  add_foreign_key "consulta", "pacientes"
+  add_foreign_key "enderecos", "pacientes"
 end
